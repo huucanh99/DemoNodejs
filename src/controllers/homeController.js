198699528ce4b2094259d1b04bf376f941d8d097
huucanh99 <huucanh28092015@gmail.com>
2024-10-12 11:48:43 +0700

    create a user in database

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,18 +1,34 @@
 import db from '../models/index';
+import CRUDservice from '../services/CRUDservice';
+
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
         return res.render('homePages.ejs', {
             data: JSON.stringify(data)
         });
-    } catch (error) {
-        console.log(e)
+    } catch (error) {  // Thay 'e' thành 'error' cho đồng nhất
+        console.log(error);  // Sửa 'e' thành 'error' để in ra lỗi
     }
-}
+};
+
 let getAboutPage = (req, res) => {
-    return res.render('about.ejs')
-}
+    return res.render('about.ejs');
+};
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+};
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDservice.createNewUser(req.body)
+    console.log(message)
+    return res.send('post crud from server');
+};
+
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-} 
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+};
---
+++ src/services/CRUDservice.js
@@ -0,0 +1,38 @@
+import db from '../models/index';
+import bcrypt from 'bcryptjs';
+const salt = bcrypt.genSaltSync(10);
+
+
+let createNewUser = async (data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPasswordFromBcrypt = await HashUserPassword(data.password);
+            await db.User.create({
+                email: data.email,
+                password: hashPasswordFromBcrypt,
+                firstName: data.firstName,
+                lastName: data.lastName,
+                address: data.address,
+                phonenumber: data.phonenumber,
+                gender: data.gender === '1' ? true : false,
+                roleId: data.roleId,
+            })
+            resolve('ok create a  new user succeed!')
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+let HashUserPassword = (password) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let hashPassword = await bcrypt.hashSync(password, salt);
+            resolve(hashPassword)
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+module.exports = {
+    createNewUser: createNewUser,
+}
