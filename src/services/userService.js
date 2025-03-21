a04027c4c45c758358d702e1842b75c897823c11
huucanh99 <huucanh28092015@gmail.com>
2025-03-21 14:13:37 +0700

    change some field for get name of object and show in create user p3

--- src/services/userService.js
+++ src/services/userService.js
@@ -11,7 +11,7 @@ let handleUserLogin = (email, password) => {
             let isExist = await checkUserEmail(email);
             if (isExist) {
                 let user = await db.User.findOne({
-                    attributes: ['email', 'roleId', 'password'],
+                    attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                     where: { email: email },
                     raw: true,
                 })
@@ -106,8 +106,9 @@ let createNewUser = async (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phonenumber: data.phonenumber,
-                    gender: data.gender === '1' ? true : false,
+                    gender: data.gender,
                     roleId: data.roleId,
+                    positionId: data.positionId
                 })
                 resolve({
                     errCode: 0,
